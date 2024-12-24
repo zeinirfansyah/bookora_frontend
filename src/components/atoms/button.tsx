4a9086edc8f1cb4e49b4cf6f4e95e73e07c123e1
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-12-24 23:10:47 +0700

    refactor [button.tsx]: clean up the classname

--- src/components/atoms/button.tsx
+++ src/components/atoms/button.tsx
@@ -14,17 +14,17 @@ export const Button = ({
   variant = "blue",
 }: IButtonProps) => {
   const variants = {
-    blue: "bg-gradient-to-r to-blue-500 via-blue-400 from-blue-500 bg-size-200 bg-pos-0",
-    red: "bg-gradient-to-r to-red-500 via-red-400 from-red-500 bg-size-200 bg-pos-0",
+    blue: "text-white bg-gradient-to-r to-blue-500 via-blue-400 from-blue-500 ",
+    red: "text-white bg-gradient-to-r to-red-500 via-red-400 from-red-500",
     green:
-      "bg-gradient-to-r to-green-500 via-green-400 from-green-500 bg-size-200 bg-pos-0",
+      "text-white bg-gradient-to-r to-green-500 via-green-400 from-green-500",
   };
 
   return (
     <>
       <button
         onClick={onClick}
-        className={`${className} ${variants[variant]} rounded px-4 py-2 border-none transition-all duration-700 hover:bg-pos-100 text-white w-full lg:w-auto`}
+        className={`${className} ${variants[variant]} bg-size-200 bg-pos-0 hover:bg-pos-100 rounded px-4 py-2 border-none transition-all duration-700 w-full lg:w-auto`}
       >
         {children}
       </button>
